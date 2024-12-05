c21247e144f1d337e76194f3e33cc5cf58ac5785
FlorinCjc <florinncjc@gmail.com>
2024-12-05 18:17:21 +0000

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl: "http://localhost:8082/api",
+  appName: "ArtEvents Page",
+  appOwner: "Florin Cojocariu",
+  appLogo: "https://media.istockphoto.com/id/1196172395/photo/in-the-photo-studio-with-professional-equipment-portrait-of-the-famous-photographer-holding.jpg?s=612x612&w=0&k=20&c=utO4aHRyA5ZUAYxbk9NelmhR1_E5-AOWUWcqDMP-NXE=",
+  itemCategories: ['PORTOFOLIU', 'ALBUME','PACHETE_FOTO_VIDEO','NONE','SEDINTA_FOTO'],
+  user: {
+    id: 1,
+    name: "Admin",
+    address: "Str. Sperantei, nr. 3",
+    email: "admin@admin.com",
+    password: "admin",
+    phone: "0765443564",
+    userRole: "ADMIN",
+    commandList: []
+  }
+};
